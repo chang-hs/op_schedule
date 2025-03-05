70d2bf5dc65a2fff4d896ade7737bfcca5d91ec5
Hansoo Chang <chang@socrate.jp>
2025-03-05 10:13:21 +0900

    Add some validators to OpForm

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import InputRequired, Length
 
 class OpForm(FlaskForm):
     patient_id = StringField('patient_id', validators=[InputRequired(), Length(min=8, max=8)])
-    name = StringField('name')
-    diagnosis = StringField('diagnosis')
+    name = StringField('name', validators=[InputRequired()])
+    diagnosis = StringField('diagnosis', validators=[InputRequired()])
     op_duration = StringField('op_duration')
     urgency = IntegerField('urgency')
     submit = SubmitField('Submit')
