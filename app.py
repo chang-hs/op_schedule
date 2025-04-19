9ce2734050a0c48fbe8d48d4dc2705d41cc1266b
Hansoo Chang <chang@socrate.jp>
2025-04-19 22:02:37 +0900

    Add email to list_op.html

--- app.py
+++ app.py
@@ -5,13 +5,20 @@ from flask import render_template
 from models import Op, User
 from forms import OpForm, EditOpForm, LoginForm
 from db import db_session
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///op_schedule.db'
-app.config['SECRET_KEY'] = 'hansoo77jp'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///op_schedule.db"
+app.config["SECRET_KEY"] = "hansoo77jp"
 
 bootstrap = Bootstrap5(app)
 
@@ -20,16 +27,19 @@ locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 def set_user_password(username, password):
     new_user = User(username=username, password=generate_password_hash(password))
     db_session.add(new_user)
     db_session.commit()
+
 
 def update_user_password(username, password):
     user = User.query.filter_by(username=username).first()
@@ -37,45 +47,56 @@ def update_user_password(username, password):
         user.password = generate_password_hash(password)
     db_session.commit()
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and check_password_hash(user.password, form.password.data):
             login_user(user)
-            return redirect(url_for('list_ops'))
+            return redirect(url_for("list_ops"))
         else:
-            flash('Invalid username or password', 'danger')
-            return redirect(url_for('login'))
-    return render_template('login.html', form=form)
+            flash("Invalid username or password", "danger")
+            return redirect(url_for("login"))
+    return render_template("login.html", form=form)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello, world!'
+    return "Hello, world!"
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 @login_required
 def register():
     form = OpForm()
     if form.validate_on_submit():
-        new_op = Op(patient_id=form.patient_id.data, name=form.name.data, age=form.age.data, diagnosis=form.diagnosis.data,
-                    memo=form.memo.data, op_duration=form.op_duration.data, urgency=form.urgency.data,
-                    phone=form.phone.data, email=form.email.data)
+        new_op = Op(
+            patient_id=form.patient_id.data,
+            name=form.name.data,
+            age=form.age.data,
+            diagnosis=form.diagnosis.data,
+            memo=form.memo.data,
+            op_duration=form.op_duration.data,
+            urgency=form.urgency.data,
+            phone=form.phone.data,
+            email=form.email.data,
+        )
         db_session.add(new_op)
         db_session.commit()
-        return redirect(url_for('register'))
-    return render_template('register.html', form=form)
+        return redirect(url_for("register"))
+    return render_template("register.html", form=form)
 
 
-@app.route('/list')
+@app.route("/list")
 @login_required
 def list_ops():
     ops = Op.query.all()
-    return render_template('list_op.html', title="List of Ops", ops=ops)
+    return render_template("list_op.html", title="List of Ops", ops=ops)
 
-@app.route('/edit/<int:op_id>', methods=['GET', 'POST'])
+
+@app.route("/edit/<int:op_id>", methods=["GET", "POST"])
 @login_required
 def edit_op(op_id):
     op = db_session.query(Op).filter_by(id=op_id).first()
@@ -92,16 +113,18 @@ def edit_op(op_id):
         op.date_set = form.date_set.data
         op.patient_notified = form.patient_notified.data
         op.orders_committed = form.orders_committed.data
-        
-        db_session.commit()
-        return redirect(url_for('list_ops'))
-    return render_template('edit_op.html', form=form)
 
-@app.route('/logout')
+        db_session.commit()
+        return redirect(url_for("list_ops"))
+    return render_template("edit_op.html", form=form)
+
+
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=8001)
--- forms.py
+++ forms.py
@@ -1,37 +1,55 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, DateField, BooleanField, SubmitField, PasswordField
+from wtforms import (
+    StringField,
+    IntegerField,
+    DateField,
+    BooleanField,
+    SubmitField,
+    PasswordField,
+)
 from wtforms.validators import InputRequired, Length, Optional
 
+
 class OpForm(FlaskForm):
-    patient_id = StringField('patient_id', validators=[InputRequired(), Length(min=8, max=8)])
-    name = StringField('name', validators=[InputRequired()])
-    age = IntegerField('age', validators=[Optional()])
-    diagnosis = StringField('diagnosis', validators=[InputRequired()])
-    op_duration = StringField('op_duration')
-    urgency = IntegerField('urgency')
-    memo = StringField('memo')
-    phone = StringField('phone')
-    email = StringField('email')
-    submit = SubmitField('Submit')
+    patient_id = StringField(
+        "patient_id", validators=[InputRequired(), Length(min=8, max=8)]
+    )
+    name = StringField("name", validators=[InputRequired()])
+    age = IntegerField("age", validators=[Optional()])
+    diagnosis = StringField("diagnosis", validators=[InputRequired()])
+    op_duration = StringField("op_duration")
+    urgency = IntegerField("urgency")
+    memo = StringField("memo")
+    phone = StringField("phone")
+    email = StringField("email")
+    submit = SubmitField("Submit")
+
 
 class EditOpForm(FlaskForm):
-    id = StringField('id')
-    patient_id = StringField('patient_id', validators=[InputRequired(), Length(min=8, max=8)])
-    name = StringField('name', validators=[InputRequired()])
-    age = IntegerField('age', validators=[Optional()])
-    phone = StringField('phone', validators=[Optional()])
-    diagnosis = StringField('diagnosis', validators=[InputRequired()])
-    op_duration = StringField('op_duration')
-    urgency = IntegerField('urgency')
-    memo = StringField('memo')
-    op_date = DateField('op_date', validators=[Optional()])
-    preop_date = DateField('preop_date',validators=[Optional()])
-    date_set = BooleanField('date_set')
-    patient_notified = BooleanField('patient_notified')
-    orders_committed = BooleanField('orders_committed')
-    submit = SubmitField('Submit')
+    id = StringField("id")
+    patient_id = StringField(
+        "patient_id", validators=[InputRequired(), Length(min=8, max=8)]
+    )
+    name = StringField("name", validators=[InputRequired()])
+    age = IntegerField("age", validators=[Optional()])
+    phone = StringField("phone", validators=[Optional()])
+    diagnosis = StringField("diagnosis", validators=[InputRequired()])
+    op_duration = StringField("op_duration")
+    urgency = IntegerField("urgency")
+    memo = StringField("memo")
+    op_date = DateField("op_date", validators=[Optional()])
+    preop_date = DateField("preop_date", validators=[Optional()])
+    date_set = BooleanField("date_set")
+    patient_notified = BooleanField("patient_notified")
+    orders_committed = BooleanField("orders_committed")
+    submit = SubmitField("Submit")
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=20)])
-    submit = SubmitField('Login')
+    username = StringField(
+        "Username", validators=[InputRequired(), Length(min=4, max=20)]
+    )
+    password = PasswordField(
+        "Password", validators=[InputRequired(), Length(min=6, max=20)]
+    )
+    submit = SubmitField("Login")
